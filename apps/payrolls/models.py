cc622e51c5001fbf6352e75e21e5499a7c3bd521
ntaliarojas <llerlynatisrojaspuertas@gmail.com>
2024-03-20 17:38:54 -0500

    Modelos TransferBank y TransferCash y los asocié con PayrollPeriod

--- apps/payrolls/models.py
+++ apps/payrolls/models.py
@@ -1,6 +1,4 @@
-# django
 from django.db import models
-
 
 class PayrollPeriod(models.Model):
     """Payroll Period model"""
@@ -8,7 +6,50 @@ class PayrollPeriod(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     type = models.CharField(max_length=100)
-
-    company = models.IntegerField(default=0, blank=True) # foreign key
-
+    company = models.IntegerField(default=0, blank=True)  # Foreign key
     is_open = models.BooleanField(default=False)
+
+
+class PayrollAccountingTransaction(models.Model):
+    """Payroll Accounting Transaction model"""
+    employee = models.IntegerField()  # ID del empleado
+    type_concept = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    amount = models.IntegerField()
+    reason = models.CharField(max_length=255)
+    total = models.IntegerField()
+    date = models.DateTimeField()
+    payroll_period = models.ForeignKey(PayrollPeriod, on_delete=models.CASCADE)  # Clave foránea a PayrollPeriod
+
+    def __str__(self):
+        return f"PayrollAccountingTransaction: {self.id} - Employee ID: {self.employee}"
+
+
+class TransferBank(models.Model):
+    """Transfer Bank model"""
+    employee = models.IntegerField()  # ID del empleado
+    date = models.DateTimeField()
+    bank = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=100)
+    reason = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    payroll_period = models.ForeignKey(PayrollPeriod, on_delete=models.CASCADE)  # Clave foránea a PayrollPeriod
+
+    def __str__(self):
+        return f"TransferBank: {self.id} - Employee ID: {self.employee}"
+
+
+class TransferCash(models.Model):
+    """Transfer Cash model"""
+    employee = models.IntegerField()  # ID del empleado
+    date = models.DateTimeField()
+    reason = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    payroll_period = models.ForeignKey(PayrollPeriod, on_delete=models.CASCADE)  # Clave foránea a PayrollPeriod
+
+    def __str__(self):
+        return f"TransferCash: {self.id} - Employee ID: {self.employee}"
