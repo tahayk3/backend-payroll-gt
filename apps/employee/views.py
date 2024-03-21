ad5d6083925344c48f90c424afb4680ba0c87255
yadiracastillo <betzycastillo14@gmail.com>
2024-03-21 11:47:23 -0600

    feat:model-employee-correction

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .serializer import EmployeeSerializer, SalaryIncreaseSerializer
+from .serializers import EmployeeSerializer, SalaryIncreaseSerializer
 from .models import Employee, SalaryIncrease
 
 
