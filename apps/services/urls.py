cc24d57dc88dafc199f61e7dadaef5154a744be8
JMizraim <jomizraim47@gmail.com>
2024-03-25 18:38:26 -0600

    Update URLs for CMS service and API endpoints

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 from apps.services.views import EmployeeCMSAPIView
 
 urlpatterns = [
-    path("cms/employees", EmployeeCMSAPIView.as_view(), name="cms-employees"),
+    path("cms/employees/", EmployeeCMSAPIView.as_view(), name="cms-employees"),
 ]
