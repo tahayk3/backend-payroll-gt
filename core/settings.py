76503e48d79e943ccec0055460c858163cfd3a4e
tahayk3 <tahayk3@gmail.com>
2024-03-25 20:26:36 -0600

    identacion

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
 
 # Se configuran las opciones de CORS
     
-    CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
